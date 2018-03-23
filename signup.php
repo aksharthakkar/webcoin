2fd698eaecdd1f71bc41a8ccb91b64e2f5cc7327
akshar <aksharkthakkar@yahoo.com>
2018-03-23 17:41:55 +0530

    new project

---
+++ signup.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Safety score</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" sizes="96x96" href="images/122.png">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="css/style.css" rel='stylesheet' type='text/css' />
+    </head>
+    <body>
+        <section class="register">
+            <div class="row">
+                <div class="container">
+                    <div class=" vendor registration-form ">
+                    <div class="row text-center">
+                        <h2>Registration</h2>
+                    </div>
+                    <div class="row">
+                        <div class="">
+                            <form>
+                                <div class="row form-group">
+                                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                                        <label>First name</label>
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                                        <label>Last name</label>
+                                        <input type="text" class="form-control" placeholder="Last name">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                                        <label>Email ID</label>
+                                    <input type="email" class="form-control" placeholder="Email ID">
+                                    </div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control" placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="row text-center form-group">
+                                    <input type="button" class="btn btn-success" value="Submit">
+                                </div>
+                                <div class="row new-link">
+                                    <h4>Already have an account ? <a href="login.html">Login</a></h4>
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>		
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
